c3a4b5a0f548ceea8c2081a1d98d77ec738d43ff
diegoohumberto <diego.151512@gmail.com>
2017-11-02 16:39:32 -0200

    Alterações ainda em progresso

--- Views/view.anamneses.php
+++ Views/view.anamneses.php
@@ -24,10 +24,125 @@
         <div class="form-group col-md-12 mensagens-retorno">
             <div class="alert alert-success">Cadastro realizado com sucesso!</div>
         </div>
-
-        <div class="col-md-12 anamnese-div">
+        
+        <!--Modal create de Anamneses-->
+        <div class="col-md-12 anamnese-div modal-create">
             <div class="container"><h5 class="obrigatorios">* Campos Obrigatórios</h5></div>
-            <form action="" method="POST" class="form_anamnese">
+            <!--Form create de Anamneses-->
+            <form action="" method="POST" class="form_anamnese form-create">
+                <input type="hidden" name="callback" value="anamnese">
+                <div class="form-group col-md-6">
+                    <br>
+                    <label>* Aluno</label>
+                    <select name="idalunos_cliente" class="form-control">
+                        <option value="0">SELECIONE</option>
+                        <?php
+                        $ReadAlunos = new Read;
+                        $ReadAlunos->ExeRead("alunos_cliente");
+                        foreach ($ReadAlunos->getResult() as $e):
+                            extract($e);
+                            echo "<option value={$idalunos_cliente}>{$idalunos_cliente} - {$nome_aluno}</option>";
+                        endforeach;
+                        ?>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <br>
+                    <label>* Peso</label>
+                    <input type="number" name="peso_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <br>
+                    <label>* Altura</label>
+                    <input type="number" name="altura_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <br>
+                    <label>* IMC</label>
+                    <input type="number" name="imc_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Pescoço</label>
+                    <input type="number" name="pescoco_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Ombro</label>
+                    <input type="number" name="ombro_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Torax</label>
+                    <input type="number" name="torax_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Abdome</label>
+                    <input type="number" name="abdome_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Cintura</label>
+                    <input type="number" name="cintura_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Quadril</label>
+                    <input type="number" name="quadril_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Braço D.</label>
+                    <input type="number" name="bd_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Braço E.</label>
+                    <input type="number" name="be_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>B.E. Contraido</label>
+                    <input type="number" name="bec_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>B.D. Contraido</label>
+                    <input type="number" name="bdc_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Antebraço E.</label>
+                    <input type="number" name="aec_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Antebraço D.</label>
+                    <input type="number" name="adc_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Coxa Esquerda</label>
+                    <input type="number" name="ce_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Coxa Direita</label>
+                    <input type="number" name="cd_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Pamturilha E.</label>
+                    <input type="number" name="pe_anamnese" class="form-control">
+                </div>
+                <div class="form-group col-md-2">
+                    <label>Panturilha D.</label>
+                    <input type="number" name="pd_anamnese" class="form-control col-md-2">
+                </div>
+                <div class="form-group col-md-4">
+
+                </div>
+                <div class="form-group col-md-12">
+                    <label>Observações</label>
+                    <textarea name="obs_anamnese" class="form-control"></textarea>
+                </div>
+                <div class="form-group col-md-12">
+                    <button type="submit" name="cadastrar" class="btn btn-primary"><i class="glyphicon glyphicon-floppy-disk"></i> Cadastrar</button>
+                </div>
+            </form>
+        </div>
+        
+        <!--Modal update de Anamneses-->
+        <div class="col-md-12 modal-update">
+            <div class="container"><h5 class="obrigatorios">* Campos Obrigatórios</h5></div>
+            <!--Form update de Anamneses-->
+            <form action="" method="POST" class="form_anamnese form-update">
                 <input type="hidden" name="callback" value="anamnese">
                 <div class="form-group col-md-6">
                     <br>
