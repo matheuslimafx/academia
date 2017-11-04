ccee02b831b393e9741979d5e232fbab7fdbc138
diegoohumberto <diego.151512@gmail.com>
2017-11-04 13:41:25 -0200

    Foi alterada as classes do formulário de pesquisa para uma especifica de cada tabela, adicionado  a função de pesquisa a aba Anamneses, e feito algumas alterações no CSS da aba anamneses, e no arquivo style.css da pasta views

--- Controllers/controller.anamnese.php
+++ Controllers/controller.anamnese.php
@@ -10,78 +10,78 @@ $jSon = array();
 $getPost = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 //Verifica se os dados vindos do arquivo js são referentes a função pesquisa por anamnese. Caso o tamanho do array seja igual a 1 é referente a pesquisa. Caso o array seja maior que 1 significa que são dados para realizar o CRUD.
-if(count($getPost) == 1):
-    
+if (count($getPost) == 1):
+
     $getQuery = array_keys($getPost);
 
     $queryPesquisa = (is_int($getQuery[0]) ? $getQuery[0] : strip_tags(str_replace('_', ' ', $getQuery[0])));
-    
+
     $buscarAnamnese = new Read;
-    
-    if(is_int($queryPesquisa)):
+
+    if (is_int($queryPesquisa)):
         $buscarAnamnese->FullRead("SELECT anamneses.idanamneses, anamneses.idalunos_cliente, alunos_cliente.nome_aluno "
                 . "FROM anamneses "
                 . "LEFT JOIN alunos_cliente ON anamneses.idalunos_cliente = alunos_cliente.idalunos_cliente "
                 . "WHERE anamneses.idalunos_cliente = {$queryPesquisa}");
         $jSon = $buscarAnamnese->getResult();
-    elseif(is_string($queryPesquisa)):
+    elseif (is_string($queryPesquisa)):
         $buscarAnamnese->FullRead("SELECT anamneses.idanamneses, anamneses.idalunos_cliente, alunos_cliente.nome_aluno "
                 . "FROM anamneses "
                 . "LEFT JOIN alunos_cliente ON anamneses.idalunos_cliente = alunos_cliente.idalunos_cliente "
                 . "WHERE alunos_cliente.nome_aluno LIKE '%{$queryPesquisa}%'");
         $jSon = $buscarAnamnese->getResult();
     endif;
-endif;
-
-//PRIMEIRA CONDIÇÃO  - NESSA CONDIÇÃO VERIFICA, SE O INDICE CALLBACK FOI PREENCHIDO:
-if (empty($getPost['callback'])):
-    //CASO NÃO HAJA O INDICE CALLBACK UM GATILHO DE ERRO (TRIGGER) É CRIADO NO ARRAY $jSon:
-    $jSon['trigger'] = "<div class 'alert alert warning'>Ação não selecionada!</div>";
 else:
 
-    //CASO O CALLBACK ESTEJA CORRETO A FUNÇÃO ARRAY_MAP INICIA A 'LIMPEZA' DOS VALORES DE CADA INDICE RETIRANDO TAGS DE SQL INJECTION E OUTRAS AMEAÇAS:
-    $Post = array_map("strip_tags", $getPost);
+//PRIMEIRA CONDIÇÃO  - NESSA CONDIÇÃO VERIFICA, SE O INDICE CALLBACK FOI PREENCHIDO:
+    if (empty($getPost['callback'])):
+        //CASO NÃO HAJA O INDICE CALLBACK UM GATILHO DE ERRO (TRIGGER) É CRIADO NO ARRAY $jSon:
+        $jSon['trigger'] = "<div class 'alert alert warning'>Ação não selecionada 1!</div>";
+    else:
 
-    //A VARIAVEL $Action É CRIADA PARA RECEBER O ACTION DO ARRAY QUE VEIO DO JS:
-    $Action = $Post['callback'];
+        //CASO O CALLBACK ESTEJA CORRETO A FUNÇÃO ARRAY_MAP INICIA A 'LIMPEZA' DOS VALORES DE CADA INDICE RETIRANDO TAGS DE SQL INJECTION E OUTRAS AMEAÇAS:
+        $Post = array_map("strip_tags", $getPost);
 
-    //O INDICE 'CALLBACK' E O SEU RESPECTIVO VALOR SÃO DESMEMBRADOS DA VARIAVEL POST, ISSO É NECESSÁRIO PARA ENVIAR PARA O BANCO APENAS OS DADOS NECESSÁRIOS:
-    unset($Post['callback']);
+        //A VARIAVEL $Action É CRIADA PARA RECEBER O ACTION DO ARRAY QUE VEIO DO JS:
+        $Action = $Post['callback'];
 
-    //SWITCH SERÁ AS CONDIÇÕES VERIFICADAS E USADAS PARA TOMAR AÇÕES DE ACORDO COM CADA CALLBACK:
-    switch ($Action):
-        //CONDIÇÃO 'anamnese' ATENDIDA:
-        case 'anamnese':
-            //CRIAÇÃO DE UMA VARIAVEL REPONSAVEL POR RECEBER O NOME DA TABELA QUE SERPA INSERIDA OS DADOS
-            $Tabela = "anamneses";
+        //O INDICE 'CALLBACK' E O SEU RESPECTIVO VALOR SÃO DESMEMBRADOS DA VARIAVEL POST, ISSO É NECESSÁRIO PARA ENVIAR PARA O BANCO APENAS OS DADOS NECESSÁRIOS:
+        unset($Post['callback']);
 
-            //INSERIR A CLASSE DA MODEL RESPONSAVEL PELA INTERAÇÃO COM O BANCO DE DADOS:
-            require '../Models/model.anamnese.php';
+        //SWITCH SERÁ AS CONDIÇÕES VERIFICADAS E USADAS PARA TOMAR AÇÕES DE ACORDO COM CADA CALLBACK:
+        switch ($Action):
+            //CONDIÇÃO 'anamnese' ATENDIDA:
+            case 'anamnese':
+                //CRIAÇÃO DE UMA VARIAVEL REPONSAVEL POR RECEBER O NOME DA TABELA QUE SERPA INSERIDA OS DADOS
+                $Tabela = "anamneses";
 
-            //INSTANCIA DO OBJETO DA CLASSE ANAMNESE RESPONSAVEL POR CADASTRAR NOVAS ANAMNESES:
-            $CadastrarAnamnese = new Anamnese;
+                //INSERIR A CLASSE DA MODEL RESPONSAVEL PELA INTERAÇÃO COM O BANCO DE DADOS:
+                require '../Models/model.anamnese.php';
 
-            //METODO DA CLASSE ANAMNESE REPONSAVEL POR CADASTRAR NOVAS ANAMNESES:
-            $CadastrarAnamnese->novoAnamnese($Tabela, $Post);
+                //INSTANCIA DO OBJETO DA CLASSE ANAMNESE RESPONSAVEL POR CADASTRAR NOVAS ANAMNESES:
+                $CadastrarAnamnese = new Anamnese;
 
-            //CONDIÇÃO PARA VERIFICAR SE FOI CADASTRADO UMA NOVA ANAMNESE, UTILIZANDO UM METODO DA CLASSE ANAMNESE:
-            if ($CadastrarAnamnese->getResult()):
-                //CONFIGURANDO UM GATILHO DE SUCESSO AO EXECUTAR O CADASTRO, TAL GATILHO SERÁ INTERPRETADO PELO JS:
-                $jSon['sucesso'] = true;
+                //METODO DA CLASSE ANAMNESE REPONSAVEL POR CADASTRAR NOVAS ANAMNESES:
+                $CadastrarAnamnese->novoAnamnese($Tabela, $Post);
 
-                //GATILHO QUE SERÁ INTERPRETADO PELO ARQUIVO JS PARA LIMPAR OS CAMPOS DO FORMULÁRIO APÓS CADASTRO:
-                $jSon['clear'] = true;
-            endif;
+                //CONDIÇÃO PARA VERIFICAR SE FOI CADASTRADO UMA NOVA ANAMNESE, UTILIZANDO UM METODO DA CLASSE ANAMNESE:
+                if ($CadastrarAnamnese->getResult()):
+                    //CONFIGURANDO UM GATILHO DE SUCESSO AO EXECUTAR O CADASTRO, TAL GATILHO SERÁ INTERPRETADO PELO JS:
+                    $jSon['sucesso'] = true;
 
-            break;
-        //CASO O CALLBACK NÃO SEJA ATENDIDO O DEFAULT SETA O GATILHO DE ERRO (TRIGGER) RESPONSAVEL POR RETORNAR O ERRO AO JS:
-        default :
-            $jSon['trigger'] = "<div class='alert alert-warning'>Ação não selecionada!</div>";
-            break;
-    endswitch;
+                    //GATILHO QUE SERÁ INTERPRETADO PELO ARQUIVO JS PARA LIMPAR OS CAMPOS DO FORMULÁRIO APÓS CADASTRO:
+                    $jSon['clear'] = true;
+                endif;
 
+                break;
+            //CASO O CALLBACK NÃO SEJA ATENDIDO O DEFAULT SETA O GATILHO DE ERRO (TRIGGER) RESPONSAVEL POR RETORNAR O ERRO AO JS:
+            default :
+                $jSon['trigger'] = "<div class='alert alert-warning'>Ação não selecionada 2!</div>";
+                break;
+        endswitch;
+
+    endif;
 endif;
-
 echo json_encode($jSon);
 ?>
 
