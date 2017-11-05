2702ae5554fe4a48f65fa6ffe94e494f00c6440e
diegoohumberto <diego.151512@gmail.com>
2017-11-05 16:16:58 -0200

    Foi adicionado a função de pesquisa na aba de Fornecedores

--- Controllers/controller.fornecedor.js
+++ Controllers/controller.fornecedor.js
@@ -1,14 +1,47 @@
 //INICIAR JQUERY:
-$(function (){
+$(function () {
+
+    $(".pesquisar-fornecedor").keyup(function () {
+        var termo = $(".pesquisar-fornecedor").val();
+        if (termo == '') {
+            termo = '0';
+        }
+        $.ajax({
+            url: "Controllers/controller.fornecedor.php",
+            data: termo,
+            type: 'POST',
+            dataType: 'json',
+            beforeSend: function (xhr) {
+
+            },
+            success: function (data) {
+                $('.j-result-fornecedores').html('');
+                $(data).each(function (index, value) {
+                    $('.j-result-fornecedores').append(
+                            "<tr id='"+ value.idfornecedores +"'>"+
+                            "<td>"+ value.idfornecedores +"</td>"+
+                            "<td>"+ value.nome_forn +"</td>"+
+                            "<td>"+ value.nome_fantasia_forn +"</td>"+
+                            "<td>"+ value.telefone_forn +"</td>"+
+                            "<td>"+
+                            "<button class='btn btn-success btn-xs open-modal-update'><i class='glyphicon glyphicon-edit'></i></button> "+
+                            "</td>"+
+                            "</tr>"
+                            );
+                });
+            }
+        });
+    });
+
 //    SELECIONAR O FORMULARIO AO SER SUBMETIDO USANDO UMA CLASSE PARA IDENTIFICAR O FORMULÁRIO:
-    $(".form_forn").submit(function(){
+    $(".form_forn").submit(function () {
 //        VARIAVEL FORM RECEBE O PROPRIO FORMULARIO USANDO O METODO DO JQUERY "THIS":
         var Form = $(this);
 //        VARIAVEL ACTION RECEBE O VALOR DO CALLBACK QUE É UM INPUT ESCONDIDO NO FORMULARIO ESSE CALLBACK SERVE COMO GATILHO PARA CONDIÇÕES:
         var Action = Form.find('input[name="callback"]').val();
 //        VARIAVEL DATA RECEBE UMA MATRIZ COM OS DADOS DO FORMULARIO (FORM) INDICE E VALOR:
         var Data = Form.serialize();
-        
+
 //        INICIAÇÃO DO AJAX - PARA ENVIAR E RECEBER DADOS:
         $.ajax({
 //            URL É O CAMINHO QUE ESTÁ A CONTROLLER, ONDE SERÃO ENVIADOS OS DADOS DO FORMULARIO (FORM -> DATA):
@@ -19,28 +52,26 @@ $(function (){
             type: 'POST',
 //            DATATYPE É O TIPO DE DADOS TRAFÉGADOS:
             dataType: 'json',
-            
 //            BEFORE SEND É A FUNÇÃO QUE PERMITE EXECUTAR UM ALGORITMO DO JQUERY ANTES DOS DADOS SEREM ENVIADOS:
             beforeSend: function (xhr) {
 //                PODE-SE NESSA PARTE MOSTRAR E RETIRAR POR EXEMPLO ELEMENTOS DO HTML:
                 alert('enviou');
             },
-            
 //            SUCCESS É A FUNÇÃO DO AJAX RESPONSÁVEL POR EXECUTAR ALGORITMOS DEPOIS QUE OS DADOS RETORNAM DA CONTROLLER, TAIS DADOS PODEM SER ACESSADOS PELA VARIAVEL "(data)":
-            success: function (data){
-                
+            success: function (data) {
+
 //                NESSA PARTE É INTERESSANTE EXECUTAR AÇÕES DE ACORDO COM OS RESULTADOS VINDOS DA CONTROLER UTILIZANDO CONDIÇÕES:
                 alert('voltou');
-                
-                if(data.sucesso){
-                   $('.alert-success').fadeIn();
+
+                if (data.sucesso) {
+                    $('.alert-success').fadeIn();
                 }
-                if(data.clear){
+                if (data.clear) {
                     Form.trigger('reset');
                 }
             }
         });
-        
+
 //        RETURN É A FUNÇÃO PARA NÃO PERMITIR QUE O FORMULÁRIO GERE AÇÃO: 
         return false;
     });
